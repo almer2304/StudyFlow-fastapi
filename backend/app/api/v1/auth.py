6aa65c935e2e8f1e0f80373e8ee7cf09c0ef3753
amee <muhammadalmer2304@gmail.com>
2026-01-13 19:42:47 +0700

    Continue with a real project structur / step 1

---
+++ backend/app/api/v1/auth.py
@@ -0,0 +1,9 @@
+from fastapi import APIRouter
+
+router = APIRouter(prefix="/auth", tags=["Auth"])
+
+@router.get("/health")
+def auth_health():
+    return {
+        "auth": "ok"
+    } 
--- backend/app/main.py
+++ backend/app/main.py
@@ -1,17 +1,16 @@
 from fastapi import FastAPI
-from app.api.v1.health import router as healt_router
+from app.db.session import engine
+from app.db.base import Base
 
-app = FastAPI(
-    title="StudyFlow API",
-    description="Backend for StudyFlow app",
-    version="0.1.0"
-)
+from app.api.v1.auth import router as auth_router
 
-app.include_router(healt_router, prefix="/api/v1")
+app = FastAPI(title="StudyFlow API")
+
+Base.metadata.create_all(bind=engine)
 
 @app.get("/")
 def root():
-    return {
-        "message": "StudyFlow API is running"
-    }
+    return {"message": "StudyFlow API running"}
+
+app.include_router(auth_router, prefix="/api/v1")
 
